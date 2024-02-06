7ef669e399fbd105969c25957e26d00e40d6bcd3
Marcos-fs <159043587+Marcos-fs@users.noreply.github.com>
2024-02-06 10:24:26 -0300

    Delete Sistema de Loja de erva mate/Loja de erva mate directory

--- Sistema de Loja de erva mate/Loja de erva mate/public/js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("quantidade").addEventListener("change", () => {
-    const quantidade = parseInt(document.getElementById("quantidade").value);
-    const precoProd = parseInt(document.getElementById("precoProd").value);
-    const total = quantidade * precoProd;
-    document.getElementById("total").value = "R$ " + total + ",00";
-});
